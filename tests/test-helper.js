f6b225f2ec06fddc7eccaf272bfe606b6d9549ac
Joan Karadimov <joan.karadimov@gmail.com>
2022-09-13 23:28:18 +0300

    Initial Commit from Ember CLI v4.7.0

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'mamaslon/app';
+import config from 'mamaslon/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
